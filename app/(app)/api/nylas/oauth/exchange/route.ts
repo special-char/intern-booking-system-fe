17669da9080246120eb55ec497eb335ceb549d20
imyagnesh <yagnesh@tcules.com>
2025-05-09 16:23:58 +0530

    Add Nylas integration and update dependencies
    - Introduced Nylas API integration with new routes for authentication and token exchange.
    - Added `nylas` dependency to `package.json` and `package-lock.json`.
    - Implemented OAuth2 flow in the Nylas routes for handling authorization and token retrieval.
    - Enhanced package-lock with new dependencies related to Nylas functionality.

---
+++ app/(app)/api/nylas/oauth/exchange/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server";
+import Nylas from "nylas";
+
+const nylasConfig = {
+  clientId: process.env.NYLAS_CLIENT_ID,
+  callbackUri: `${process.env.NEXT_PUBLIC_SERVER_URL}/api/nylas/oauth/exchange`,
+  apiKey: process.env.NYLAS_API_KEY,
+  apiUri: process.env.NYLAS_API_URI,
+};
+
+const nylas = new Nylas({
+  apiKey: nylasConfig.apiKey!,
+  apiUri: nylasConfig.apiUri,
+});
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const code = searchParams.get("code");
+
+  if (!code) {
+    return NextResponse.json(
+      { error: "No authorization code returned from Nylas" },
+      { status: 400 }
+    );
+  }
+
+  console.log("Code:", code);
+
+  try {
+    const response = await nylas.auth.exchangeCodeForToken({
+      clientSecret: nylasConfig.apiKey,
+      clientId: nylasConfig.clientId!,
+      redirectUri: nylasConfig.callbackUri,
+      code,
+    });
+
+    const { grantId } = response;
+
+    console.log("Grant ID:", grantId);
+
+    return NextResponse.json({
+      message: "OAuth2 flow completed successfully for grant ID: " + grantId,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to exchange authorization code for token" },
+      { status: 500 }
+    );
+  }
+}
