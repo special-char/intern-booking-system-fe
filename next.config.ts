c66772282e311f1d37b6980fcd619b793e36abc4
Nirav <niravp@tcules.com>
2025-05-21 18:03:23 +0530

    Add webpack configuration to ignore specific modules

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import { withPayload } from "@payloadcms/next/withPayload";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^pg-native$|^cloudflare:sockets$/,
+      })
+    );
+
+    return config;
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
