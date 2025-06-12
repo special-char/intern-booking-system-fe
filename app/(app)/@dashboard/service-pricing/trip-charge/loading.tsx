980fe4e49f79cd5a915253e0b3acde14b1c58722
Yagnesh Modh <yagnesh.modh@gmail.com>
2025-06-12 11:41:50 +0530

    remove unnessesary routes

--- app/(app)/@dashboard/service-pricing/trip-charge/loading.tsx
+++
@@ -1,7 +0,0 @@
-import TripChargeFormSkeleton from "@/modules/service-pricing/trip-charge/components/form/skeleton";
-
-export default function TripChargeLoading() {
-  return (
-    <TripChargeFormSkeleton />
-  )
-}
